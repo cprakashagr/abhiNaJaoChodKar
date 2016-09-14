fa0b98ec4cfe253a6ffd2d72ca868763ed1dfb11
Chandra Prakash <cprakashagr@gmail.com>
2016-09-15 02:19:30 +0530

    Initial list of Static Filters

--- notification-guru/__init__.py
+++ notification-guru/__init__.py
@@ -9,24 +9,25 @@ gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 
-
 APP_INDICATOR_ID = 'abhiNaJaoChodKar'
+INITIAL_FILTERS = ['gnome-settings-daemon']
 
-messages = []
-indicator = None
 
 class NotificationGuru:
-
     def __init__(self):
-        indicator = appindicator.Indicator.new(APP_INDICATOR_ID, os.path.abspath('ic_alarm_add_white_24dp.png'), appindicator.IndicatorCategory.APPLICATION_STATUS)
-        indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-        indicator.set_menu(self.build_menu())
+        self.messages = []
+        self.indicator = appindicator.Indicator.new(APP_INDICATOR_ID,
+                                                    os.path.abspath('ic_alarm_add_white_24dp.png'),
+                                                    appindicator.IndicatorCategory.APPLICATION_STATUS)
+        self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+        self.indicator.set_menu(self.build_menu())
 
         DBusGMainLoop(set_as_default=True)
 
-        bus = dbus.SessionBus()
-        bus.add_match_string_non_blocking("eavesdrop=true, interface='org.freedesktop.Notifications', member='Notify'")
-        bus.add_message_filter(self.notifications)
+        self.bus = dbus.SessionBus()
+        self.bus.add_match_string_non_blocking(
+            "eavesdrop=true, interface='org.freedesktop.Notifications', member='Notify'")
+        self.bus.add_message_filter(self.notifications)
 
         gtk.main()
 
@@ -35,13 +36,13 @@ class NotificationGuru:
     def build_menu(self):
         menu = gtk.Menu()
         item_quit = gtk.MenuItem('Quit')
-        item_quit.connect('activate', quit)
+        item_quit.connect('activate', self.quit)
         menu.append(item_quit)
         menu.show_all()
         return menu
 
-
-    def quit(self, source):
+    @staticmethod
+    def quit(source):
         gtk.main_quit()
 
     def modifyAllMenuItems(self):
@@ -49,7 +50,7 @@ class NotificationGuru:
         pass
 
     def notifications(self, bus, message):
-        messages.append(message)
+        self.messages.append(message)
         self.modifyAllMenuItems()
         print([arg for arg in message.get_args_list()])
 
